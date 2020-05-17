c3ad1627e0430db89203225c2737d1e64cca2cb0
MuhannadSatouf <muhannad.satouf0016@stud.hkr.se>
2020-05-17 19:56:38 +0200

    Add employee and add customer with error handling

--- src/Controller/AddEmployeeController.java
+++ src/Controller/AddEmployeeController.java
@@ -4,8 +4,9 @@ import Models.DataBaseHandler;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
+import javafx.beans.binding.BooleanBinding;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Alert;
@@ -16,32 +17,28 @@ import java.sql.SQLException;
 import java.util.ResourceBundle;
 
 public class AddEmployeeController implements Initializable {
-    public JFXTextField address;
-    public JFXTextField phone;
-    public JFXTextField email;
+    public JFXTextField addressTextField;
+    public JFXTextField phoneTextField;
+    public JFXTextField emailTextField;
     public JFXButton saveBtn;
-    public JFXTextField name;
+    public JFXTextField nameTextField;
     public JFXButton cancelBtn;
-    public JFXTextField employeePassword;
+    public JFXTextField employeePasswordTextField;
     public JFXComboBox type;
-    public JFXTextField ssnEmployee;
-    ObservableList<String> employeeTypeList = FXCollections.observableArrayList ("Sales agent", "Buyer's agent", "Real estate support staff");
+    public JFXTextField ssnEmployeeTextField;
+
+
     DataBaseHandler dataBaseHandler;
 
-    public void saveEmployee(ActionEvent actionEvent) {
-        if (ssnEmployee.getText().isEmpty() || address.getText().isEmpty() || name.getText().isEmpty() ||
-                phone.getText().isEmpty() || email.getText().isEmpty() || employeePassword.getText().isEmpty()) {
-            Alert alert = new Alert(Alert.AlertType.ERROR);
-            alert.setHeaderText(null);
-            alert.setContentText("Please enter in all fields");
-            alert.showAndWait();
-            return;
-        }
-        String reg = "[0-9]+";
-        if (ssnEmployee.getText().matches(reg) & phone.getText().matches(reg)) {
+    public void saveEmployee(ActionEvent actionEvent) throws SQLException {
 
-            dataBaseHandler.addUser(ssnEmployee.getText(), employeePassword.getText(),
-                    name.getText(), address.getText(), phone.getText(), email.getText());
+        DataBaseHandler dataBaseHandler = new DataBaseHandler();
+        String check = dataBaseHandler.checkIfUserExist(ssnEmployeeTextField.getText());
+
+        if (!check.equals(ssnEmployeeTextField.getText())) {
+
+            dataBaseHandler.addUser(ssnEmployeeTextField.getText(), employeePasswordTextField.getText(),
+                    nameTextField.getText(), addressTextField.getText(), phoneTextField.getText(), emailTextField.getText());
 
             try {
 
@@ -50,32 +47,91 @@ public class AddEmployeeController implements Initializable {
 
                 PreparedStatement pst;
                 pst = DataBaseHandler.connection.prepareStatement(qu);
-                pst.setString(1, type.getValue().toString());
-                pst.setString(2, ssnEmployee.getText());
+                pst.setString(1, "Employee");
+                pst.setString(2, ssnEmployeeTextField.getText());
 
                 pst.execute();
                 pst.close();
 
+                createMessage("Thank your, the employee has been added to the system");
             } catch (SQLException throwable) {
                 throwable.printStackTrace();
             }
-        } else {
-            Alert alert = new Alert(Alert.AlertType.INFORMATION);
-            alert.setHeaderText(null);
-            alert.setContentText("You should enter numbers only in SSN and Phone fields!");
-            alert.showAndWait();
-
+        } else if (check.equals(ssnEmployeeTextField.getText())) {
+            createMessage("The user already exist");
         }
     }
 
     public void cancel(ActionEvent actionEvent) {
-        System.exit(0);
+        employeePasswordTextField.setText("");
+        phoneTextField.setText("");
+        emailTextField.setText("");
+        nameTextField.setText("");
+        addressTextField.setText("");
+        employeePasswordTextField.setText("");
+        ssnEmployeeTextField.setText("");
+
     }
 
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        dataBaseHandler = DataBaseHandler.getInstance ();
-        type.setItems (employeeTypeList);
+        makeSSNOnlyNumbers();
+        makeTheSaveButtonActive();
+        makeNameOnlyString();
+        dataBaseHandler = DataBaseHandler.getInstance();
+
+    }
+
+    private void makeSSNOnlyNumbers() {
+        // make the textField ssn and phone take only numbers.
+        ssnEmployeeTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d") && newValue.length() <= 12) {
+                ssnEmployeeTextField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+        phoneTextField.textProperty().addListener((observable, oldValue, newValue) -> {
+            if (!newValue.matches("\\d*")) {
+                phoneTextField.setText(newValue.replaceAll("[^\\d]", ""));
+            }
+        });
+    }
+
+    public void createMessage(String message) {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        alert.showAndWait();
+    }
+
+    // make ths save button active when all fields are filled
+    public void makeTheSaveButtonActive() {
+        BooleanBinding bb = new BooleanBinding() {
+            {
+                super.bind(ssnEmployeeTextField.textProperty(), employeePasswordTextField.textProperty(), phoneTextField.textProperty()
+                        , emailTextField.textProperty(), nameTextField.textProperty(), addressTextField.textProperty(),
+                        nameTextField.textProperty());
+            }
+
+            @Override
+            protected boolean computeValue() {
+                return (ssnEmployeeTextField.getText().isEmpty() || employeePasswordTextField.getText().isEmpty()
+                        || phoneTextField.getText().isEmpty() || emailTextField.getText().isEmpty() || nameTextField.getText().isEmpty()
+                        || addressTextField.getText().isEmpty() || nameTextField.getText().isEmpty());
+            }
+        };
+
+        saveBtn.disableProperty().bind(bb);
+    }
+
+    public void makeNameOnlyString() {
+        nameTextField.textProperty().addListener(new ChangeListener<String>() {
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+
+                if (!newValue.matches("\\D*")) {
+                    nameTextField.setText(newValue.replaceAll("[^\\D]", ""));
+                }
+            }
+        });
     }
 }
